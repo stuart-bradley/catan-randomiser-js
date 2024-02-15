e25daae6607c4a5b6eab79d5c35a4dd2ed87950b
Stuart Bradley <stuy.bradley@gmail.com>
2024-02-15 19:35:58 +0000

    Make board grid responsive

--- src/app/components/ui/BoardGrid.tsx
+++ src/app/components/ui/BoardGrid.tsx
@@ -10,7 +10,7 @@ const BoardGrid: React.FC = () => {
   const hexagons = getCubeCoords(serializedBoard);
 
   return (
-    <HexGrid className="max-w-sm mx-auto">
+    <HexGrid className="max-w-fit mx-auto">
       <Layout flat={false} spacing={1.02}>
         {hexagons.map(({ hex, style }, i) => (
           <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} style={style} />
