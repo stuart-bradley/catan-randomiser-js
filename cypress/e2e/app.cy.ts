327fc7e828f825e9d1be1360296b4d7344c891ff
Stuart Bradley <stuy.bradley@gmail.com>
2024-02-19 11:36:55 +0000

    Added basic E2E tests

---
+++ cypress/e2e/app.cy.ts
@@ -0,0 +1,44 @@
+import { ALGORITHM_COASTAL, ALGORITHM_RANDOM } from "../../src/lib/constants";
+
+describe("Form", () => {
+  it("should have all the correct input elements", () => {
+    cy.visit("http://localhost:3000/");
+
+    cy.get('[data-cy="use-seafarers-checkbox"]').should("not.be.checked");
+
+    cy.get('[data-cy="rand-algorithm-selector"] option:selected').should(
+      "have.value",
+      ALGORITHM_RANDOM,
+    );
+
+    cy.get('[data-cy="players-4-radio"]').should("be.checked");
+    cy.get('[data-cy="players-6-radio"]').should("not.be.checked");
+
+    cy.get('[data-cy="submit-button"]').should("have.text", "Generate");
+  });
+  it("should change select values when use-seafarers is checked", () => {
+    cy.visit("http://localhost:3000/");
+
+    // Assert form is in base state.
+    cy.get('[data-cy="use-seafarers-checkbox"]').should("not.be.checked");
+    cy.get('[data-cy="rand-algorithm-selector"] option:selected').should(
+      "have.value",
+      ALGORITHM_RANDOM,
+    );
+    cy.get('[data-cy="rand-algorithm-selector"] option').should(
+      "not.have.value",
+      ALGORITHM_COASTAL,
+    );
+
+    // Select the seafarers checkbox. force: true is required due to div overlaying input.
+    cy.get('[data-cy="use-seafarers-checkbox"]').check({ force: true });
+    cy.get('[data-cy="use-seafarers-checkbox"]').should("be.checked");
+
+    // Assert it's possible to select a seafarers algorithm.
+    cy.get('[data-cy="rand-algorithm-selector"]').select(ALGORITHM_COASTAL);
+    cy.get('[data-cy="rand-algorithm-selector"] option:selected').should(
+      "have.value",
+      ALGORITHM_COASTAL,
+    );
+  });
+});
--- src/app/components/ui/BoardForm.tsx
+++ src/app/components/ui/BoardForm.tsx
@@ -33,6 +33,7 @@ const BoardForm: React.FC = () => {
             {...register("useSeafarers")}
             type="checkbox"
             className="sr-only peer"
+            data-cy="use-seafarers-checkbox"
           />
           <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
           <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
@@ -42,15 +43,13 @@ const BoardForm: React.FC = () => {
       </div>
 
       <div className="mb-5">
-        <label
-          htmlFor="randAlgorithm"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
+        <label className="block text-gray-700 text-sm font-bold mb-2">
           Select a Board Generation Algorithm
         </label>
         <select
           {...register("randAlgorithm")}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          data-cy="rand-algorithm-selector"
         >
           {getAlgorithms.map((item) => (
             <option key={item.id} value={item.id}>
@@ -61,10 +60,7 @@ const BoardForm: React.FC = () => {
       </div>
 
       <fieldset className="mb-5">
-        <label
-          htmlFor="numOfPlayers"
-          className="block text-gray-700 text-sm font-bold mb-2"
-        >
+        <label className="block text-gray-700 text-sm font-bold mb-2">
           Number of Players
         </label>
         {NUM_OF_PLAYERS.map((item) => (
@@ -75,11 +71,9 @@ const BoardForm: React.FC = () => {
               className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
               type="radio"
               value={item.id}
+              data-cy={`players-${item.id}-radio`}
             />
-            <label
-              htmlFor="numOfPlayers"
-              className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
+            <label className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
               {item.label}
             </label>
           </div>
@@ -89,6 +83,7 @@ const BoardForm: React.FC = () => {
         type="submit"
         aria-disabled={pending}
         className="px-6 py-3.5 text-base w-full font-medium text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        data-cy="submit-button"
       >
         Generate
       </button>
